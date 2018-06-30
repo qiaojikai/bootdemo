a3d91e330a906a68035df3d2ad2c99e01e63bc96
qiaojikai <502072807@qq.com>
2018-06-30 19:07:04 +0800

    测试提交

--- src/main/java/com/example/demo/MywebApplication.java
+++ src/main/java/com/example/demo/MywebApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class MywebApplication {
 
-	@RequestMapping("/")
+	@RequestMapping("/helloWorld")
 	public String hello() {
 		return "helloWorld";
 	}
